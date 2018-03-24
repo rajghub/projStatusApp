b258f0566adc781601a749980b692a502ef55707
Rajesh <rajsv456@gmail.com>
2018-03-24 13:31:42 +0530

    Angular 5 update

--- src/app/project-snapshot/project-snapshot.component.ts
+++ src/app/project-snapshot/project-snapshot.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-snapshot.component.css']
 })
 export class ProjectSnapshotComponent implements OnInit {
+  title1 = 'Snapshot title';
 
   constructor() { }
 
